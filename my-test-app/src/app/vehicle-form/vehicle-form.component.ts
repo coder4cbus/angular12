d2a14811f923007750de128e4b6224c0035bcda9
Eileen Ulry <coder4cbus@gmail.com>
2022-06-14 20:36:49 -0400

    Lab6-checkedPlustLab7-start

--- my-test-app/src/app/vehicle-form/vehicle-form.component.ts
+++ my-test-app/src/app/vehicle-form/vehicle-form.component.ts
@@ -1,7 +1,44 @@
+// import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+// import { NgForm } from '@angular/forms';
+// import { Vehicle } from '../vehicle';
+
+
+// @Component({
+//   selector: 'app-vehicle-form',
+//   templateUrl: './vehicle-form.component.html',
+//   styleUrls: ['./vehicle-form.component.css']
+// })
+// export class VehicleFormComponent implements OnInit {
+//   @Input() 
+//   vehicle = new Vehicle("", 0, "", "", 0, 0, false, [])
+//   @Input("on-cancel")
+//   @Output("on-submit")
+//   emitter = new EventEmitter
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+//   handleSubmit(nForm: NgForm) {
+//     const input = nForm.value
+//     const v = new Vehicle(
+//       input.veh_vin,
+//       input.veh_year,
+//       input.veh_make,
+//       input.veh_model,
+//       input.veh_mileage,
+//       input.veh_price,
+//       input.veh_featured === "" ? false : input.veh_featured,
+//       [])
+//       this.emitter.emit(v)
+//   }
+
+  
+
+// }
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Vehicle } from '../vehicle';
-
 
 @Component({
   selector: 'app-vehicle-form',
@@ -11,12 +48,12 @@ import { Vehicle } from '../vehicle';
 export class VehicleFormComponent implements OnInit {
   @Output("on-submit")
   emitter = new EventEmitter
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
-  handleSubmit(nForm: NgForm) {
+  handleSubmit(nForm:NgForm) {
     const input = nForm.value
     const v = new Vehicle(
       input.veh_vin,
@@ -27,7 +64,8 @@ export class VehicleFormComponent implements OnInit {
       input.veh_price,
       input.veh_featured === "" ? false : input.veh_featured,
       [])
-      this.emitter.emit(v)
+  
+    this.emitter.emit(v)
   }
 
 }
--- my-test-app/src/app/vehicle.ts
+++ my-test-app/src/app/vehicle.ts
@@ -1,10 +1,10 @@
 export class Vehicle {
-    constructor (public VIN: string, 
+    constructor(public VIN: string, 
         public year: number, 
-        public make: string,
+        public make: string, 
         public model: string,
-        public mileage: number, 
-        public price: number, 
+        public mileage: number,
+        public price: number,
         public featured: boolean,
         public photos: string[]) {
     }
