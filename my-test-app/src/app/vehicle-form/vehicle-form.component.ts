4d910dcb325e2ab4d61b90204837d5ef4742b802
Eileen Ulry <coder4cbus@gmail.com>
2022-06-15 10:03:18 -0400

    Lab7-inclCancelOption-Done

--- my-test-app/src/app/vehicle-form/vehicle-form.component.ts
+++ my-test-app/src/app/vehicle-form/vehicle-form.component.ts
@@ -1,42 +1,4 @@
-// import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-// import { NgForm } from '@angular/forms';
-// import { Vehicle } from '../vehicle';
-
-
-// @Component({
-//   selector: 'app-vehicle-form',
-//   templateUrl: './vehicle-form.component.html',
-//   styleUrls: ['./vehicle-form.component.css']
-// })
-// export class VehicleFormComponent implements OnInit {
-//   @Input() 
-//   vehicle = new Vehicle("", 0, "", "", 0, 0, false, [])
-//   @Input("on-cancel")
-//   @Output("on-submit")
-//   emitter = new EventEmitter
-//   constructor() { }
-
-//   ngOnInit(): void {
-//   }
-
-//   handleSubmit(nForm: NgForm) {
-//     const input = nForm.value
-//     const v = new Vehicle(
-//       input.veh_vin,
-//       input.veh_year,
-//       input.veh_make,
-//       input.veh_model,
-//       input.veh_mileage,
-//       input.veh_price,
-//       input.veh_featured === "" ? false : input.veh_featured,
-//       [])
-//       this.emitter.emit(v)
-//   }
-
-  
-
-// }
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Vehicle } from '../vehicle';
 
@@ -46,8 +8,15 @@ import { Vehicle } from '../vehicle';
   styleUrls: ['./vehicle-form.component.css']
 })
 export class VehicleFormComponent implements OnInit {
-  @Output("on-submit")
-  emitter = new EventEmitter
+@Input()
+vehicle = new Vehicle("", 0, "", "", 0, 0, false, [])
+@Output("on-submit")
+emitter = new EventEmitter
+
+@Input()
+canceled = false
+@Output("on-cancel")
+canceler = new EventEmitter
 
   constructor() { }
 
@@ -66,6 +35,10 @@ export class VehicleFormComponent implements OnInit {
       [])
   
     this.emitter.emit(v)
+  }
+
+  handleCancel() {
+    this.canceler.emit()
   }
 
 }
