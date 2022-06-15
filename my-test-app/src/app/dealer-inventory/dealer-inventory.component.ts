216370545b99b380664ab3c9a5fd5ec73b7ade16
Eileen Ulry <coder4cbus@gmail.com>
2022-06-15 16:23:37 -0400

    Lab10-Done!

--- my-test-app/src/app/dealer-inventory/dealer-inventory.component.ts
+++ my-test-app/src/app/dealer-inventory/dealer-inventory.component.ts
@@ -11,29 +11,34 @@ export class DealerInventoryComponent implements OnInit {
 
   vehicleToEdit?:Vehicle
 
-  inventory:Vehicle[] = []
+  
 
 
   constructor(private inventorySvc: InventoryService) { }
-
+  inventory:Vehicle[] = []
+  
   ngOnInit(): void {
-    this.inventory = this.inventorySvc.getInventory()         
+    this.inventorySvc.getInventory().subscribe(list => this.inventory = list)         
   }
   trackByVIN(index:number, car:Vehicle) : string {
     return car.VIN
   }
   deleteVehicle(car:Vehicle) {
-    this.inventorySvc.deleteVehicle(car)
-    this.inventory = this.inventorySvc.getInventory()
+    this.inventorySvc.deleteVehicle(car).subscribe(() => {
+      //Update local copy of list
+    this.inventory = this.inventory.filter(v => v.VIN !== car.VIN)
+    }) 
   }
+
   handlePhotoNavigation(photoIndex:number, car:Vehicle) {
     if (photoIndex == car.photos.length - 1) {
       alert("Come visit us in our showroom!")
     }
   }
   addVehicle(v:Vehicle) {
-    this.inventorySvc.addVehicle(v)
-    this.inventory = this.inventorySvc.getInventory()
+    this.inventorySvc.addVehicle(v).subscribe(() => {
+      this.inventory.push(v)
+    })
   }
 
   beginEditing(v:Vehicle) {
@@ -41,13 +46,10 @@ export class DealerInventoryComponent implements OnInit {
   }
   
   commitEdit(v:Vehicle) {
-    //Copy the edited data
-    this.inventorySvc.updateVehicle(this.vehicleToEdit!.VIN, v)
-    this.inventory = this.inventorySvc.getInventory()
-  
-    this.vehicleToEdit = undefined
-  
+    this.inventorySvc.updateVehicle(this.vehicleToEdit!.VIN, v).subscribe(()=> {
+      Object.assign(this.vehicleToEdit, v)
+      this.vehicleToEdit = undefined
+    })
   }
-
 
 }
