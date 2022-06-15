d1d3940578d995aa49f7c85b0698b585a93954fe
Eileen Ulry <coder4cbus@gmail.com>
2022-06-15 10:14:10 -0400

    Lab7-IncCancelWorkNotAllSaved

--- my-test-app/src/app/dealer-inventory/dealer-inventory.component.ts
+++ my-test-app/src/app/dealer-inventory/dealer-inventory.component.ts
@@ -12,49 +12,51 @@ export class DealerInventoryComponent implements OnInit {
 
   vehicleToEdit?:Vehicle
 
-  inventory:Vehicle[] = [
-    {
-      VIN: "Y123",
-      year: 2012,
-      make: "HONDA",
-      model: "Civic",
-      mileage: 70000,
-      price: 5900.00,
-      featured: false,
-      photos: []
-    },
-    {
-      VIN: "P1023",
-      year: 2019,
-      make: "BMW",
-      model: "328i",
-      mileage: 42000,
-      price: 12000.00,
-      featured: true,
-      photos: ["/assets/b-1.png", "/assets/b-2.png", "/assets/b-3.png", "/assets/b-4.png"]
-    },
-    {
-      VIN: "NM182",
-      year: 2018,
-      make: "KIA",
-      model: "Niro",
-      mileage: 31000,
-      price: 7900.00,
-      featured: false,
-      photos: ["/assets/k-1.png", "/assets/k-2.png", "/assets/k-3.png"]
+  inventory:Vehicle[] = []
 
-    },
-    {
-      VIN: "Y187",
-      year: 2014,
-      make: "HONDA",
-      model: "Accord",
-      mileage: 40000,
-      price: 8900.00,
-      featured: false,
-      photos: []
-    },
-  ]
+  // inventory:Vehicle[] = [
+  //   {
+  //     VIN: "Y123",
+  //     year: 2012,
+  //     make: "HONDA",
+  //     model: "Civic",
+  //     mileage: 70000,
+  //     price: 5900.00,
+  //     featured: false,
+  //     photos: []
+  //   },
+  //   {
+  //     VIN: "P1023",
+  //     year: 2019,
+  //     make: "BMW",
+  //     model: "328i",
+  //     mileage: 42000,
+  //     price: 12000.00,
+  //     featured: true,
+  //     photos: ["/assets/b-1.png", "/assets/b-2.png", "/assets/b-3.png", "/assets/b-4.png"]
+  //   },
+  //   {
+  //     VIN: "NM182",
+  //     year: 2018,
+  //     make: "KIA",
+  //     model: "Niro",
+  //     mileage: 31000,
+  //     price: 7900.00,
+  //     featured: false,
+  //     photos: ["/assets/k-1.png", "/assets/k-2.png", "/assets/k-3.png"]
+
+  //   },
+  //   {
+  //     VIN: "Y187",
+  //     year: 2014,
+  //     make: "HONDA",
+  //     model: "Accord",
+  //     mileage: 40000,
+  //     price: 8900.00,
+  //     featured: false,
+  //     photos: []
+  //   },
+  // ]
 
 
   constructor() { }
@@ -78,6 +80,9 @@ export class DealerInventoryComponent implements OnInit {
 
   beginEditing = (v:Vehicle) => {
     this.vehicleToEdit = v
+  }
+
+  handleSubmit = (v:Vehicle) => {
   }
   
   commitEdit = (v:Vehicle) => {
