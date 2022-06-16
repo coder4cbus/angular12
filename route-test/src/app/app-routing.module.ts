d07210bdbedce6b6b2a3a12b26626a899c053527
Eileen Ulry <coder4cbus@gmail.com>
2022-06-16 09:59:35 -0400

    Lab12SPAwRouting-REALLYDone!

--- route-test/src/app/app-routing.module.ts
+++ route-test/src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
 import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
